9fb2b2a3462ce0e3c358c6ec4a2caf844266d2f0
Giovani <g186397@dac.unicamp.br>
2024-04-08 13:47:38 -0300

    finished all relevant functions. Now to add sockets.

--- song.c
+++ song.c
@@ -23,19 +23,19 @@ struct song createSong() {
     struct song newSong;
 
     char title[50];
-    scanf('Song title: %s',title);
+    scanf("Song title: %s",title);
     char interpret[20];
-    scanf('Song interpret: %s',interpret);
+    scanf("Song interpret: %s",interpret);
     char language[20];
-    scanf('Song language: %s',language);
+    scanf("Song language: %s",language);
     char type[20];
-    scanf('Song type: %s',type);
+    scanf("Song type: %s",type);
     char chorus[100];
-    scanf('Song chorus: %s',chorus);
+    scanf("Song chorus: %s",chorus);
     int identifier;
-    scanf('Song identifier: %d', identifier);
+    scanf("Song identifier: %d", identifier);
     int year;
-    scanf('Song year: %d', year);
+    scanf("Song year: %d", year);
     newSong.identifier = identifier;
     newSong.year = year;
     strncpy(newSong.title, title, 50);
@@ -48,6 +48,7 @@ struct song createSong() {
 }
 
 struct song searchSong(int identifier, struct songList* songList) {
+    
     struct songList* searcher = songList;
     while (searcher != NULL) {
         if (searcher->song.identifier == identifier) {
@@ -63,33 +64,76 @@ struct song searchSong(int identifier, struct songList* songList) {
 
 
 void registerSong(struct songList* songList) {
+
     struct song newSong = createSong();
     struct songList* node = (struct songList*)malloc(sizeof(struct songList));
     node->song = newSong;
     node->next = songList;
     songList = node;
 
+    return;
+
 }
 
 void removeSong(int identifier, struct songList* songList) {
 
+    struct songList* searcher = songList;
+    struct songList* previous;
+    while (searcher != NULL) {
+        if (searcher->song.identifier == identifier) {
+            previous->next = searcher->next;
+            free(searcher);
+            return;
+        }
+        previous = searcher;
+        searcher = searcher->next;
+    }
+
+    printf("Song not found. No songs were removed.");
+    return songList->song;
 }
 
-void listSongsByYear(struct songList* songList) {
+void listSongsByYear(struct songList* songList, int year) {
+    
+    struct songList* searcher = songList;
+    while (searcher != NULL) {
+        if (searcher->song.year == year) {
+            listSong(searcher->song);
+        }
+        searcher = searcher->next;
+    }
 
+    printf("All registered songs from the year %d have been listed.", year);
+    return songList->song;
 }
 
 void listSongsByYearAndLanguage(struct songList* songList, int year, char* language) {
+    struct songList* searcher = songList;
+    while (searcher != NULL) {
+        if (searcher->song.year == year && strcmp(searcher->song.language, language) == 0) {
+            listSong(searcher->song);
+        }
+        searcher = searcher->next;
+    }
 
+    printf("All registered %s songs from the year %d have been listed.", language, year);
+    return songList->song;
 }
 
 void listSongsByType(struct songList* songList, char* type) {
+    struct songList* searcher = songList;
+    while (searcher != NULL) {
+        if (strcmp(searcher->song.type, type) == 0) {
+            listSong(searcher->song);
+        }
+        searcher = searcher->next;
+    }
 
+    printf("All registered songs of type %s have been listed.", type);
+    return songList->song;
 }
 
-void listSong(struct songList* songList, int identifier) {
-
-    struct song song = searchSong(identifier, songList);
+void listSong(struct song song) {
 
     printf("Song identifier: %d", song.identifier);
     printf("Song title: %s", song.title);
@@ -98,15 +142,18 @@ void listSong(struct songList* songList, int identifier) {
     printf("Song type: %s", song.type);
     printf("Song chorus: %s", song.chorus);
     printf("Song year: %d", song.year);
+
+    return;
 }
 
 void listAllSongs(struct songList* songList) {
 
     struct songList* songListCopy = songList;
     while (songListCopy != NULL) {
-        listSong(songListCopy, songListCopy->song.identifier);
+        listSong(songListCopy->song);
         songListCopy = songListCopy->next;
     }
+    return;
 }
 
 void downloadSong(struct songList* songList, int identifier) {
@@ -116,7 +163,7 @@ void downloadSong(struct songList* songList, int identifier) {
 int main() {
     struct songList* songList = NULL;
     registerSong(songList);
-    listSong(songList, 1);
+    listSong(searchSong(1, songList));
     registerSong(songList);
 
     return 0;
